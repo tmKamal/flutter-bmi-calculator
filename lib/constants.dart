70538f24d0a78a07708153bd19ca06ab8012e8b9
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2020-09-10 16:35:21 +0530

    separate constant page for manage constants.

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const kCardBodyTextColor = Color(0xFF8D8E98);
+const kCardLabelText = TextStyle(color: kCardBodyTextColor, fontSize: 18.0);
+const kCardNumberText = TextStyle(fontSize: 50.0, fontWeight: FontWeight.w900);
+
+const kBottomContainerHeight = 80.0;
+const kBottomBtnPinkishColor = Color(0xFFEB1555);
+const kActiveCardColor = Color(0xFF1D1E33);
+const kInactiveCardColor = Color(0xFF111328);
